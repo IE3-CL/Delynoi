3c5674d7c84a4155d38aa85967ac9f600486e710
ppizarror <pablo@ppizarror.com>
2022-11-18 16:59:47 +0000

    Merge functor classes cpp

--- Delynoi/src/models/generator/functions/functions.cpp
+++ Delynoi/src/models/generator/functions/functions.cpp
@@ -1,13 +1,113 @@
 #pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedStructInspection"
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
 #pragma ide diagnostic ignored "modernize-concat-nested-namespaces"
 
-#include <Delynoi/models/generator/Functor.h>
 #include <Delynoi/models/generator/functions/RandomDouble.h>
 #include <Delynoi/models/generator/functions/RandomInteger.h>
-#include "FunctorClasses.cpp"
+#include <Delynoi/models/generator/Functor.h>
+#include <Delynoi/utilities/delynoi_utilities.h>
+#include <Delynoi/utilities/UniqueList.h>
 
 namespace Delynoi {
+    class Constant : public Functor {
+    public:
+        Constant() = default;
+
+        inline double apply(double x) override { return x; }
+    };
+
+    class Uniform : public Functor {
+    private:
+        double delta;
+    public:
+        explicit Uniform(double delta) { this->delta = delta; }
+
+        inline double apply(double x) override { return x * delta; }
+    };
+
+    class Sine : public Functor {
+    private:
+        double amplitude;
+        double frecuency;
+        double phase;
+    public:
+        Sine(double a, double f, double p) {
+            this->amplitude = a;
+            this->frecuency = f;
+            this->phase = p;
+        }
+
+        inline double apply(double x) override {
+            return amplitude * std::sin(utilities::radian(frecuency * x * 180) + utilities::radian(phase));
+        };
+    };
+
+    class Cosine : public Functor {
+    private:
+        double amplitude;
+        double frecuency;
+        double phase;
+    public:
+        Cosine(double a, double f, double p) {
+            this->amplitude = a;
+            this->frecuency = f;
+            this->phase = p;
+        }
+
+        inline double apply(double x) override {
+            return amplitude * std::cos(utilities::radian(frecuency * x * 180) + utilities::radian(phase));
+        };
+    };
+
+    class DisplaceDelta : public Functor {
+    private:
+        double delta;
+        bool alternating = false;
+    public:
+        explicit DisplaceDelta(double delta) {
+            this->delta = delta;
+        }
+
+        inline double apply(double x) override {
+            alternating = !alternating;
+
+            if (alternating) {
+                return x + delta / 2;
+            } else {
+                return x;
+            }
+        }
+    };
+
+    class ConstantAlternating : public Functor {
+    private:
+        UniqueList<double> visitedPlaces;
+        double delta{};
+        bool alternating = false;
+    public:
+        ConstantAlternating() = default;
+
+        inline double apply(double x) override {
+            if (visitedPlaces.size() == 1) {
+                delta = std::abs(visitedPlaces[0] - x);
+            }
+
+            int index = visitedPlaces.push_back(x);
+
+            if (index < visitedPlaces.size() - 1) {
+                alternating = !alternating;
+                visitedPlaces.clear();
+            }
+
+            if (alternating) {
+                return x + delta / 2;
+            } else {
+                return x;
+            }
+        }
+    };
+
     namespace functions {
         Functor *constant() {
             return new Uniform(1.0);
