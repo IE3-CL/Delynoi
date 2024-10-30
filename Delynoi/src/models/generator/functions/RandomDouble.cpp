4c3221007392ff35bff19dd78ade8ba2bd5713a7
Pablo Pizarro R. <pablo@ppizarror.com>
2024-10-30 11:32:28 -0300

    Fix generator for macos

--- Delynoi/src/models/generator/functions/RandomDouble.cpp
+++ Delynoi/src/models/generator/functions/RandomDouble.cpp
@@ -8,7 +8,8 @@ std::mt19937 Random_Double::rng(rd());
 Random_Double::Random_Double(const double min, const double max) {
     this->min = min;
     this->max = max;
-    this->uni = std::uniform_real_distribution(min, max);
+    // ReSharper disable once CppTemplateArgumentsCanBeDeduced
+    this->uni = std::uniform_real_distribution<double>(min, max); // Template required for MacOS
 }
 
 double Random_Double::apply(double x) {
